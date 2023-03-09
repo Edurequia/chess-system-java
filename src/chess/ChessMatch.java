9756a08f1ab75e7fcdbb70e23135b3c27792ee6b
Edurequia <edurequia@gmail.com>
2023-03-09 16:23:55 -0300

    Possible movies of a piece

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -44,6 +44,9 @@ public class ChessMatch {
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source position");
 		}
+		if (!board.piece(position).isThereAnyPossibleMove()) {
+			throw new ChessException("Theres is not possible moves for the chosen pieces");
+		}
 	}
 	
 	
